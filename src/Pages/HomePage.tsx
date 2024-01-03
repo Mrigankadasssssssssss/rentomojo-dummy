2f9f2e3593d715dbfd18357319324c1a9abaec51
Mriganka Das <dasmriganka.2002@gmail.com>
2024-01-03 12:09:26 +0530

    first commit

---
+++ src/Pages/HomePage.tsx
@@ -0,0 +1,13 @@
+"use client"
+import Home from '@/Components/Home/Home'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <Home/>
+    </div>
+  )
+}
+
+export default HomePage
