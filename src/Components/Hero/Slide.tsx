be708cff354bf5adc8b7912ebbd1261f67f9f48c
souviksardar2003 <souviksardar2003@hotmail.com>
2024-01-04 14:53:00 +0530

    Hero Banner

---
+++ src/Components/Hero/Slide.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import React from "react";
+import './slide.css';
+
+interface propsType {
+  img: string;
+}
+
+const Slide: React.FC<propsType> = ({ img}) => {
+
+  return (
+    <div className="slide">
+        <Image
+        priority
+        style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: "xl" }}
+        src={img}
+        alt="banner"
+        width={2000}
+        height={2000}
+      />
+    </div>
+    
+      
+  );
+};
+
+export default Slide;
