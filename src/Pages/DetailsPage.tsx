875beb35ba922afa4b8927ce9167bcc00e31b705
Mriganka Das <dasmriganka.2002@gmail.com>
2024-01-07 20:47:06 +0530

    Details

---
+++ src/Pages/DetailsPage.tsx
@@ -0,0 +1,12 @@
+import Details from '@/Components/Details/Details'
+import React from 'react'
+
+const DetailsPage = () => {
+  return (
+    <div>
+      <Details/>
+    </div>
+  )
+}
+
+export default DetailsPage
